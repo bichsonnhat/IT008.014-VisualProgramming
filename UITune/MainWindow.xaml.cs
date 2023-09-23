7835b0303594d5e59e0771643dd811f4d325900c
Bich Son Nhat <bichsonnhat@gmail.com>
2023-09-24 01:24:14 +0700

    Add LoginWindow

--- UITune/MainWindow.xaml.cs
+++ UITune/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,12 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using UITune.Views.LoginWindow;
 
 namespace UITune
 {
@@ -20,9 +25,11 @@ namespace UITune
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
         }
+        
     }
 }
