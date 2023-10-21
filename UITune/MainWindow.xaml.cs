fb7f189db4c13e48cb9390a8184a37cc1b21a107
Bich Son Nhat <bichsonnhat@gmail.com>
2023-10-21 19:03:04 +0700

    fix

--- UITune/MainWindow.xaml.cs
+++ UITune/MainWindow.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +9,9 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Threading;
-using UITune.Views.LoginWindow;
 
 namespace UITune
 {
@@ -25,11 +20,9 @@ namespace UITune
     /// </summary>
     public partial class MainWindow : Window
     {
-        
         public MainWindow()
         {
             InitializeComponent();
         }
-        
     }
 }
---
+++ UITune/ViewModel/LoginVM/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using UITune.Views.LoginWindow;
+
+namespace UITune.ViewModel.LoginVM
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public bool isLoaded = false;
+        public LoginViewModel() 
+        {
+            if (!isLoaded)
+            {
+                isLoaded = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }
+        }
+    }
+}
