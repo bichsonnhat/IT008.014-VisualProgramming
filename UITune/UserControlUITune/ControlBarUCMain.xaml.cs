f68764e32bc5ed8d1643f213e7ab4371d230302f
Bich Son Nhat <bichsonnhat@gmail.com>
2023-10-22 23:21:56 +0700

    add user screen

---
+++ UITune/UserControlUITune/ControlBarUCMain.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UITune.ViewModel.ControlBarVM;
+
+namespace UITune.UserControlUITune
+{
+    /// <summary>
+    /// Interaction logic for ControlBarUCMain.xaml
+    /// </summary>
+    public partial class ControlBarUCMain : UserControl
+    {
+        public ControlBarViewModel ViewModel { get; set; }
+        public ControlBarUCMain()
+        {
+            InitializeComponent();
+            this.DataContext = ViewModel = new ControlBarViewModel();
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            btn.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#595757");
+        }
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            btn.Background = new SolidColorBrush(Colors.Transparent);
+        }
+        private void Button_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            btn.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#ff0000");
+        }
+        private void Button_MouseLeave_1(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.Background = new SolidColorBrush(Colors.Transparent);
+        }
+    }
+}
---
+++ UITune/ViewModel/ButtonViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UITune.ViewModel
+{
+    public class ButtonViewModel : RadioButton
+    {
+        static ButtonViewModel()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ButtonViewModel), new FrameworkPropertyMetadata(typeof(ButtonViewModel)));
+        }
+    }
+}
